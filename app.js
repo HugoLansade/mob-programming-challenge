da36744fa929f4ba5f0ee60792bd511c7860cc11
Hugo Lansade <lhugo@lansade.com>
2021-10-14 11:21:04 +0200

    Iteration 6

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 require("dotenv").config();
 require('./config/mongodb')
 console.log("Hello World !")
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", hbs);
+app.set("views", __dirname + "/views");
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.listen(process.env.PORT, () => console.log(`Server launched at http://localhost:${process.env.PORT}`));
+
+const mainRouter = require(("./routers/main.js"));
+app.use(mainRouter);
---
+++ routers/main.js
@@ -0,0 +1,39 @@
+// const { request } = require("express");
+const { application, request } = require("express");
+const express = require("express");
+const router = new express.Router();
+
+const images = [
+    "/img/greg-rakozy-oMpAz-DN-9I-unsplash.jpg", 
+    "/img/joshua-aragon-FGXqbqbGt5o-unsplash.jpg", 
+    "/img/kevin-ku-aiyBwbrWWlo-unsplash.jpg" ]
+
+const users = [
+    {name : "keridou",
+    email : "simon@lekeridou",
+    favoriteLangage : "foot"
+    }
+]
+
+
+const homeController = (request, result) => {
+    result.render("home.hbs", images);
+}
+
+const squadController = (request, result)=> {
+    result.render("allUsers.hbs", users);
+}
+
+const addController = (request, result) => {
+    result.render("formUser.hbs");
+}
+
+
+
+router.get("/", homeController);
+router.get("/my-dev-squad", squadController);
+router.get("/add-new-ironhacker", addController);
+
+
+
+module.exports = router;
